10368574de256e8616342985bb52fbbd04b6d09e
geehhyy <devkhun409@gmail.com>
2023-05-12 11:45:09 +0900

    스프링 빈과 의존관계 > 컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,4 +1,4 @@
-package hello.hellospring;
+package hello.hellospring; // 해당 패키지 하위만 Component 스캔이 가능함
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
